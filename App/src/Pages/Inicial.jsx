8f40e486223e28a73b2f2b5bce81cc845e0d6cd0
luis <luisfelippe55482@gmail.com>
2025-08-06 08:25:02 -0300

    att

--- App/src/Pages/Inicial.jsx
+++ App/src/Pages/Inicial.jsx
@@ -1,101 +1,72 @@
-import enviar from "../imgs/enviar.png"
-import photo from "../imgs/photo.png"
-import compartilhar from "../imgs/compartilhar.png"
-import salvar from "../imgs/salvar.png"
-import coracao from "../imgs/coraao.png"
-import "../styles/style.css"
-import  '../styles/inicio.css'
-import { useState,useEffect } from "react"
-function Inicial(){
-const [user,setUser]= useState([])
+import enviar from "../imgs/enviar.png";
+import photo from "../imgs/photo.png";
+import compartilhar from "../imgs/compartilhar.png";
+import salvar from "../imgs/salvar.png";
+import coracao from "../imgs/coraao.png";
+import user1 from "../imgs/user1.png";
+import "../styles/style.css";
+import "../styles/inicio.css";
+import { useState, useEffect } from "react";
+function Inicial() {
+const num = [1,2,3,4,5];
 
-async function usuario() {
-  const res = await fetch(`https://randomuser.me/api/?results=1&gender=male`);
-  const data = await res.json();
-
-  const lista = data.results.map(usera => ({
-    nome: `${usera.name.first} ${usera.name.last}`,
-    username: usera.login.username,
-    foto: usera.picture.medium,
-  }));
-
-  setUser(lista);
-}usuario()
+const [pessoas,setPessoas] = useState([])
+useEffect(()=>{
+    async function perfis(){
+       const res = await fetch(`https://randomuser.me/api/?results=1&gender=male`)
+        const data = await res.json();
+        setPessoas(data)
+    }perfis()
+},[])
+console.log(pessoas.results[0].location.city)
 
 
-const [text,setText]= useState([])
-async function texto(){
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=3`)
-    const data = await res.json()
-    setText(data)
-}
-useEffect(() => {
-  texto();
-}, []);
-//https://jsonplaceholder.typicode.com/posts
+git 
+  return (
+    <div className="inicio">
+      <div className="header" style={{}}>
+        <h3>
+          <span>V</span>oxer Spa<span>c</span>e
+        </h3>
+        <img src={user1} alt="" />
+      </div>
 
+      <div className="feed">
 
-    
+       {num.map((_, index) =>(
 
-    return(
-        <div className="inicio">
-            
-<div className="header">
-   <h3>
-    <span>V</span>
-        <br />
-        S
-   </h3>
-    <div className="post">
-       <div className="pesquisa-container">
-         <input type="text"
-         placeholder="Escreva suas ideias"
-         />
-         <img src={enviar} alt="" />
-       </div>
-        <button>
-           <img src={photo} alt="" />
-         
-        </button>
-    </div>
-</div>
-
-<div className="feed">
-    <div className="posts">
-        {user.map((usera,i)=>(
-      <div className="header-poster" key={i}>
-
-            <img  src={usera.foto} alt={usera.username} />
-            <div className="user-header">
-                <p>{usera.nome}</p>
-                <p id="ahh">{usera.username}</p>
-            </div>
-        </div>
-
-        ))}
-            {text.map((post, index)=> (
-  <div className="post-msg" key="index">
-            <p>{post.body}</p>
-        </div>
-
-            ))}
       
-        <div className="controle-posts">
-            <button>
-                adicionar Comentario
-            </button>
-           <div className="fds">
-             <img src={coracao} alt="curtir" />
-            <img src={salvar} alt="salvar" />
-            <img src={compartilhar} alt="compartilhar" />
-           </div>
+        <div className="posts" key={index}>
+          <div className="header-poster">
+            <img src="https://randomuser.me/api/portraits/men/76.jpg" />
+            <div className="user-header">
+              <p>luisaodamassa</p>
+              <p id="ahh">luisacds</p>
+            </div>
+          </div>
+
+          <div className="post-msg" key="index">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
+              vero autem delectus officiis non natus maiores tempore veritatis
+              dolorum quidem. Beatae vel ex plicabo reprehenderit doloremque
+              iure, voluptate blanditiis ex veritatis.
+            </p>
+          </div>
+
+          <div className="controle-posts">
+            <button>adicionar Comentario</button>
+            <div className="fds">
+              <img src={coracao} alt="curtir" />
+              <img src={salvar} alt="salvar" />
+              <img src={compartilhar} alt="compartilhar" />
+            </div>
+          </div>
         </div>
+ ))}
+      </div>
+      <div className="footer"></div>
     </div>
-
-
-</div>
-<div className="footer"></div>
-        </div>
-    )
+  );
 }
-export default Inicial
+export default Inicial;
